3ef01678c4149de8a7f3b37812081d348e03d223
Anmol-singh-coder <anmol4102003@gmail.com>
2025-11-28 00:15:23 +0530

    Time: 6 ms (28.96%), Space: 49.9 MB (66.67%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -4,9 +4,11 @@ class Solution {
         for(int i=0;i<items.size();i++){
             if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)){
                 count++;
+                continue;
             }
             if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)){
                 count++;
+                continue;
             }
             if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)){
                 count++;
